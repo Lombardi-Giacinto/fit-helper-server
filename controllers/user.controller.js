47d43c720f49f19df3f64bbe0629dffcd4e8b10c
Lombardi-Giacinto <65731516+Lombardi-Giacinto@users.noreply.github.com>
2025-10-08 15:15:20 +0200

    Google cookie test5

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,18 +1,21 @@
 import User from '../models/user.model.js';
 import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 const jwtSecret = process.env.JWT_SECRET;
 
 const setAuthCookie = (res, user) => {
     const token = jwt.sign({ sub: user._id, email: user.email }, jwtSecret, { expiresIn: '1h' });
-
     const cookieOptions = {
         httpOnly: true,
         secure: true,
         sameSite: 'None',
         maxAge: 60 * 60 * 1000 // 1 h
     };
-    res.cookie('access_token', token, cookieOptions);
+
+    // Usiamo res.setHeader per un controllo più esplicito e per evitare header multipli.
+    const cookieString = cookie.serialize('access_token', token, cookieOptions);
+    res.setHeader('Set-Cookie', cookieString);
 };
 
 const createUser = async (req, res) => {
@@ -85,6 +88,7 @@ const checkEmail = (req, res) => {
 const loginGoogle = (req, res) => {
     try {
         setAuthCookie(res, req.user);
+        // redirect richiesto dal flusso OAuth2
         res.redirect(`https://main.dr3pvtmhloycm.amplifyapp.com/status=success`);
     } catch (error) {
         console.error('Error during Google login process:', error);
@@ -93,7 +97,6 @@ const loginGoogle = (req, res) => {
 }
 
 const getMe = (req, res) => {
-    console.log("CCCCC",req.user);
     const userResponse = req.user.toObject();
     delete userResponse.password;
     res.status(200).json(userResponse);
